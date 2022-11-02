96def1f0b35d247abda048b776416df4ac6f0fa2
Gleb Deshin <gdeshin@internet.ru>
2022-11-03 00:04:19 +0300

    home task 3.5 done

---
+++ src/components/ProductList.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <ul class="catalog__list">
+    <ProductItem v-for="(product, index) in products" :key="index" :product="product"></ProductItem>
+  </ul>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+export default {
+  props: ["products"],
+  components: { ProductItem },
+};
+</script>
