d84c48ea8e59ae8c0c1f83f19dd04a25a264d228
Gleb Deshin <gdeshin@internet.ru>
2022-11-05 23:33:28 +0300

    alerts on load deleted

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { message, message2 } from "./data/data";
-import showMessage from "./data/func";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
-
-showMessage(message);
-showMessage(message2);
