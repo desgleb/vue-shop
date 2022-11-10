90298d7e19a8e86a56fdfecbf575123334a008a4
Gleb Deshin <gdeshin@internet.ru>
2022-11-10 13:43:48 +0300

    color list in productItem added

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -12,22 +12,10 @@
     <span class="catalog__price"> {{ product.price }} </span>
 
     <ul class="colors colors--black">
-      <li class="colors__item">
+      <li class="colors__item" v-for="itemColor in product.colors" :key="itemColor.hex">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color" />
-          <span class="colors__value" style="background-color: #73b6ea"> </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#8BE000" v-model="color" />
-          <span class="colors__value" style="background-color: #8be000"> </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" value="#222" v-model="color" />
-          <span class="colors__value" style="background-color: #222"> </span>
+          <input class="colors__radio sr-only" type="radio" :value="itemColor.hex" v-model="color" />
+          <span class="colors__value" :style="itemColor.background"> </span>
         </label>
       </li>
     </ul>
--- src/data/products.js
+++ src/data/products.js
@@ -5,6 +5,20 @@ export default [
     title: "АВТОКРАН ИВАНОВЕЦ КС-45717К-1М",
     price: 24000000,
     image: "/img/kran.jpg",
+    colors: [
+      {
+        hex: "#73B6EA",
+        background: "background-color: #73B6EA",
+      },
+      {
+        hex: "#FFBE15",
+        background: "background-color: #FFBE15",
+      },
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+    ],
   },
   {
     id: 2,
@@ -12,6 +26,20 @@ export default [
     title: "Торт Прага от Палыча",
     price: 1367,
     image: "/img/praga.jpg",
+    colors: [
+      {
+        hex: "#FFBE15",
+        background: "background-color: #FFBE15",
+      },
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+    ],
   },
   {
     id: 3,
@@ -19,6 +47,20 @@ export default [
     title: "Allroad A6",
     price: 10546090,
     image: "/img/allroad.jpg",
+    colors: [
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+    ],
   },
   {
     id: 4,
@@ -26,6 +68,20 @@ export default [
     title: "Радионяня Motorola MBP16",
     price: 3690,
     image: "/img/radio.jpg",
+    colors: [
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+    ],
   },
   {
     id: 5,
@@ -33,6 +89,20 @@ export default [
     title: "Ультразвуковая зубная щётка Playbrush Smart Sonic",
     price: 5660,
     image: "/img/toothbrush.jpg",
+    colors: [
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+      {
+        hex: "#000000",
+        background: "background-color: #000000",
+      },
+    ],
   },
   {
     id: 6,
@@ -40,6 +110,24 @@ export default [
     title: "Смартфон Xiaomi Mi Mix 3 6/128GB",
     price: 21790,
     image: "/img/phone.jpg",
+    colors: [
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+      {
+        hex: "#000000",
+        background: "background-color: #000000",
+      },
+    ],
   },
   {
     id: 7,
@@ -47,6 +135,28 @@ export default [
     title: "Электроскейт Razor Cruiser",
     price: 24690,
     image: "/img/board.jpg",
+    colors: [
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+      {
+        hex: "#000000",
+        background: "background-color: #000000",
+      },
+    ],
   },
   {
     id: 8,
@@ -54,6 +164,16 @@ export default [
     title: "Смартфон Xiaomi Mi A3 4/64GB Android One",
     price: 14960,
     image: "/img/phone-2.jpg",
+    colors: [
+      {
+        hex: "#73B6EA",
+        background: "background-color: #73B6EA",
+      },
+      {
+        hex: "#FFBE15",
+        background: "background-color: #FFBE15",
+      },
+    ],
   },
   {
     id: 9,
@@ -61,6 +181,20 @@ export default [
     title: "Смартфон Xiaomi Redmi 6/128GB",
     price: 8960,
     image: "/img/phone-3.jpg",
+    colors: [
+      {
+        hex: "#FFBE15",
+        background: "background-color: #FFBE15",
+      },
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+    ],
   },
   {
     id: 10,
@@ -68,6 +202,20 @@ export default [
     title: "Электрический дрифт-карт Razor Crazy Cart",
     price: 39990,
     image: "/img/bicycle.jpg",
+    colors: [
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+    ],
   },
   {
     id: 11,
@@ -75,6 +223,28 @@ export default [
     title: "Гироскутер Razor Hovertrax 2.0",
     price: 34900,
     image: "/img/wheels.jpg",
+    colors: [
+      {
+        hex: "#FFBE15",
+        background: "background-color: #FFBE15",
+      },
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+      {
+        hex: "#8BE000",
+        background: "background-color: #8BE000",
+      },
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+      {
+        hex: "#FFFFFF",
+        background: "background-color: #FFFFFF",
+      },
+    ],
   },
   {
     id: 12,
@@ -82,6 +252,12 @@ export default [
     title: "Детский трюковой самокат Razor Grom",
     price: 4990,
     image: "/img/scooter.jpg",
+    colors: [
+      {
+        hex: "#73B6EA",
+        background: "background-color: #73B6EA",
+      },
+    ],
   },
   {
     id: 13,
@@ -89,6 +265,12 @@ export default [
     title: "Роллерсёрф Razor RipStik Air Pro",
     price: 6690,
     image: "/img/ripstik.jpg",
+    colors: [
+      {
+        hex: "#000000",
+        background: "background-color: #000000",
+      },
+    ],
   },
   {
     id: 14,
@@ -96,6 +278,12 @@ export default [
     title: "Наушники AirPods с беспроводным зарядным футляром",
     price: 16560,
     image: "/img/airpods.jpg",
+    colors: [
+      {
+        hex: "#FF6B00",
+        background: "background-color: #FF6B00",
+      },
+    ],
   },
   {
     id: 15,
@@ -103,5 +291,11 @@ export default [
     title: "Наушники Sony",
     price: 30690,
     image: "/img/headphones.jpg",
+    colors: [
+      {
+        hex: "#939393",
+        background: "background-color: #939393",
+      },
+    ],
   },
 ];
