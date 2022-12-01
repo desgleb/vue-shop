f322044fe2976db2e0834894ecdc766ee68f6b38
Gleb Deshin <gdeshin@internet.ru>
2022-12-01 09:04:56 +0300

    6.4 WIP

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import products from "@/data/products";
 
 Vue.use(Vuex);
 
@@ -21,6 +22,16 @@ export default new Vuex.Store({
           amount,
         });
       }
+    },
+  },
+  getters: {
+    cartDetailProducts(state) {
+      return state.cartProducts.map((item) => {
+        return {
+          ...item,
+          product: products.find((p) => p.id === item.productId),
+        };
+      });
     },
   },
 });
