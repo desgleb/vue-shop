a8d59b9d65e83ebd9a073d8a1e6cf8dea5462bc1
Gleb Deshin <gdeshin@internet.ru>
2022-12-19 14:54:16 +0300

    8.1 WIP

---
+++ src/components/BaseFormTextarea.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseFormField :title="title" :error="error">
+    <input
+      v-model="dataValue"
+      class="form__input"
+      type="text"
+      name="name"
+      :placeholder="placeholder"
+    />
+  </BaseFormField>
+</template>
+
+<script>
+import BaseFormField from "./BaseFormField.vue";
+
+export default {
+  props: ["title", "error", "placeholder", "value"],
+  components: { BaseFormField },
+  computed: {
+    dataValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+};
+</script>
